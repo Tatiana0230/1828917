da05b9bb4a249acb21245f7116fb0ab430455ccf
Tatiana Castillo <sala@HOR-AULA501-005.unihorizonte.salas>
2019-10-29 15:51:55 -0500

    Mi primer commit

---
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -0,0 +1,16 @@
+
+package senasalud;
+
+import java.util.Scanner;
+
+public class SenaSalud {
+
+    public static void main(String[] args) {
+String nom;
+Scanner nombre= new Scanner(System.in);
+System.out.println("Ingrese su nombre");
+nom=nombre.next();
+System.out.println("Su nombre es:"+nom);
+    }
+    
+}
