a58186dd07d65e5ffad48269ac8ce3afda028b1e
Tatiana Castillo <sala@HOR-AULA501-005.unihorizonte.salas>
2019-10-29 16:41:56 -0500

    Mi segundo commit

--- SenaSalud/src/senasalud/SenaSalud.java
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -6,11 +6,16 @@ import java.util.Scanner;
 public class SenaSalud {
 
     public static void main(String[] args) {
-String nom;
+String nom,apelli,ed,col;
 Scanner nombre= new Scanner(System.in);
 System.out.println("Ingrese su nombre");
 nom=nombre.next();
 System.out.println("Su nombre es:"+nom);
+Scanner apellido= new Scanner(System.in);
+System.out.println("Ingrese su apellido");
+apelli=apellido.next();
+System.out.println("Su apellido es:"+apelli);
+
     }
     
 }
